d59995a8a529f5e7678a96d03442bb4881ee7542
reza <gpsgeneration46@gmail.com>
2026-03-12 22:32:25 +0700

    proses noto

---
+++ src/js/noto.js
@@ -0,0 +1,30 @@
+// 1. Siapkan Data Dummy (Array of Objects)
+let dummyNotes = [
+    {
+        id: 1,
+        title: "Pergi ke pasar membeli ikan",
+        body: "Pergi ke pasar membeli ikan, cakep Pergi ke pasar membeli ikan...",
+        isFavorite: false
+    },
+    {
+        id: 2,
+        title: "Belajar UI/UX Design Gestalt",
+        body: "Hari ini aku belajar tentang prinsip Gestalt dalam desain UI/UX...",
+        isFavorite: true // Contoh yang udah jadi favorit
+    },
+      {
+        id: 3,
+        title: "Belajar UI/UX Design Gestalt",
+        body: "Hari ini aku belajar tentang prinsip Gestalt dalam desain UI/UX...",
+        isFavorite: true // Contoh yang udah jadi favorit
+    },
+      {
+        id: 4,
+        title: "Belajar UI/UX Design Gestalt",
+        body: "Hari ini aku belajar tentang prinsip Gestalt dalam desain UI/UX...",
+        isFavorite: true // Contoh yang udah jadi favorit
+    }
+];
+
+
+
